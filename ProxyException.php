1c428e4282c6f3dc2b81f10cde5491d2ac48b199
shiwolang <shiwolang@live.com>
2015-12-07 16:12:51 +0800

    init

---
+++ ProxyException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by zhouzhongyuan.
+ * User: zhouzhongyuan
+ * Date: 2015/12/7
+ * Time: 16:00
+ */
+
+namespace shiwolang\proxy;
+
+
+class ProxyException extends \Exception
+{
+
+}
---
+++ ProxyObject.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by zhouzhongyuan.
+ * User: zhouzhongyuan
+ * Date: 2015/12/7
+ * Time: 16:09
+ */
+
+namespace shiwolang\proxy;
+
+
+class ProxyObject
+{
+
+}
